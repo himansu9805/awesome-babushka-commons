32d88b45490c1503525c37b853b8623e3da779fb
himansu9805 <himansu9805@gmail.com>
2025-07-24 20:30:06 +0530

    feat(commons): update __init__.py to include ApiAuth and bump version to 0.1.1

--- src/commons/__init__.py
+++ src/commons/__init__.py
@@ -1,7 +1,8 @@
 """Commons package for awesome-babushka projects."""
 
+from .authentication import ApiAuth
 from .database.mongo import MongoConnect
 from .models import SingletonMeta
 
-__version__ = "0.1.0"
-__all__ = ["SingletonMeta", "MongoConnect"]
+__version__ = "0.1.1"
+__all__ = ["SingletonMeta", "MongoConnect", "ApiAuth"]
